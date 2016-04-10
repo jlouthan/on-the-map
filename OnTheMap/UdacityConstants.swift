c6899f60b4b5a103d65ede7d2e29e2b51f7b0bb5
Jennifer Louthan <jenny@uncorkedstudios.com>
2016-04-10 16:25:32 -0700

    Add some Udacity Client constants for making login request

---
+++ OnTheMap/UdacityConstants.swift
@@ -0,0 +1,38 @@
+//
+//  UdacityConstants.swift
+//  OnTheMap
+//
+//  Created by Jennifer Louthan on 4/10/16.
+//  Copyright © 2016 JennyLouthan. All rights reserved.
+//
+
+import Foundation
+
+extension UdacityClient {
+    
+    //MARK: Constants
+    struct Constants {
+        // MARK: URLs
+        static let ApiScheme = "https"
+        static let ApiHost = "www.udacity.com"
+        static let ApiPath = "/api"
+    }
+    
+    //MARK: Methods
+    struct Methods {
+        static let CreateSession = "/session"
+    }
+    
+    // MARK: JSON Body Keys
+    struct JSONBodyKeys {
+        static let Udacity = "udacity"
+        static let Username = "username"
+        static let Password = "password"
+    }
+    
+    //MARK: JSON Response Keys
+    struct JSONResponseKeys {
+        static let Account = "account"
+        static let UserId = "key"
+    }
+}
