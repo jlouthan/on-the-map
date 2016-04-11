7374f1fbededcf879d844608725d7e904f057702
Jennifer Louthan <jenny@uncorkedstudios.com>
2016-04-10 19:04:13 -0700

    Add and show tab bar controller on login success

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -14,26 +14,57 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
     
     @IBAction func loginPressed(sender: AnyObject) {
         guard !emailField.text!.isEmpty && !passwordField.text!.isEmpty else {
-            print("Missing required field")
+            displayError("Missing required field")
             return
         }
         UdacityClient.sharedInstance().createSession(emailField.text!, password: passwordField.text!) { (success, error) in
             
             //TODO handle or display error better here
-            if success {
-                print("success!")
-            } else {
-                if let errorText = error!.userInfo[NSLocalizedDescriptionKey] as? String {
-                    performUIUpdatesOnMain({ 
-                        self.errorLabel.text = errorText
-                    })
+            performUIUpdatesOnMain({
+                if success {
+                    print("success!")
+                    self.completeLogin()
+                } else {
+                    let errorText = error!.userInfo[NSLocalizedDescriptionKey] as? String
+                    self.displayError(errorText)
                 }
-            }
+            })
             
         }
     }
     
+    // MARK: Login
+    
+    private func completeLogin() {
+//        let controller = storyboard!.instantiateViewControllerWithIdentifier("ManagerNavigationController") as! UINavigationController
+        let controller = storyboard!.instantiateViewControllerWithIdentifier("TabBarController")
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
+}
+
+// MARK: - LoginViewController (Configure UI)
+
+extension LoginViewController {
+    
+    private func setUIEnabled(enabled: Bool) {
+        loginButton.enabled = enabled
+        
+        // adjust login button alpha
+        if enabled {
+            loginButton.alpha = 1.0
+        } else {
+            loginButton.alpha = 0.5
+        }
+    }
+    
+    private func displayError(errorString: String?) {
+        if let errorString = errorString {
+            errorLabel.text = errorString
+        }
+    }
 }
