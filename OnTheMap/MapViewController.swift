ed4db440b0913e49093d1181f68b802c88cd3415
Jennifer Louthan <jenny@uncorkedstudios.com>
2016-04-10 19:20:23 -0700

    Add a map view to show up in the first tab on login

---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Jennifer Louthan on 4/10/16.
+//  Copyright © 2016 JennyLouthan. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet var mapView: MKMapView!
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //get and add annotations here? Maybe get them in view will appear?
+    }
+    
+    
+}
