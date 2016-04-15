f85638eba02e4185ec21731aa17a9410af2fffcb
Jennifer Louthan <jenny@uncorkedstudios.com>
2016-04-14 21:01:36 -0700

    Show successfully encoded location in a map view

--- OnTheMap/InfoPostingViewController.swift
+++ OnTheMap/InfoPostingViewController.swift
@@ -8,9 +8,44 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class InfoPostingViewController: UIViewController {
-    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var findOnMapButton: UIButton!
+    @IBOutlet weak var mediaLinkTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //MARK: Submission State
+    var mapString: String?
+    var pinLocation: CLLocationCoordinate2D?
+    
+    //MARK: Life Cycle
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        initUI()
+    }
+    
+    private func initUI() {
+        mediaLinkTextField.hidden = true
+        submitButton.hidden = true
+        locationTextField.hidden = false
+        findOnMapButton.hidden = false
+        findOnMapButton.enabled = true
+    }
+    
+    @IBAction func findOnMapPressed(sender: AnyObject) {
+        guard !locationTextField.text!.isEmpty else {
+            print("Missing Required field")
+            return
+        }
+        findOnMapButton.enabled = false
+        findOnMapButton.hidden = true
+        locationTextField.hidden = true
+        handleLocationString(locationTextField.text!)
+    }
     
     @IBAction func submitPressed(sender: AnyObject) {
         let currentStudentDict = [
@@ -26,5 +61,24 @@ class InfoPostingViewController: UIViewController {
         
         let currentStudent = StudentInformation(dictionary: currentStudentDict)
         print(currentStudent)
+    }
+    
+    private func handleLocationString(locationString: String) {
+        //TODO "display activity" here
+        CLGeocoder().geocodeAddressString(locationString) { (placemarks, error) in
+            guard error == nil else {
+                print(error)
+                return
+            }
+            if let placemark = placemarks?.first {
+                //Set pinLocation to proper coordinate and display on a map view
+                self.pinLocation = placemark.location!.coordinate
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = self.pinLocation!
+                self.mapView.hidden = false
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
 }
